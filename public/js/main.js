7ad08ce3b3c3b41adc3ed70f09a986aeec24ac55
rkrkrk <fintan@cvs3d.com>
2022-02-03 23:39:30 +0000

    fix stuff

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ console.log('QQQ REMOTE HOST IS ', remoteHost)
 const host = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') ? localHost : remoteHost;
 
 
-console.log('QQQ HOST IS '+host)
+console.log('QQQ HOST IS ' + host)
 var urlWeather = host + 'metar';
 
 var urlBuoy = host + 'buoy';
@@ -20,7 +20,7 @@ function getWeather() {
     //     "numReadings": "20"
     // };
 
-    urlWeather = urlWeather+"?code=EIDW&numReadings=20"
+    urlWeather = urlWeather + "?code=EIDW&numReadings=20"
 
     $.get(urlWeather, function (data) {
         // console.log('res', data);
@@ -41,11 +41,11 @@ function getWeather() {
         $("#eidw").append("<table class='table'>" + header + rows + "</table>");
         $("#eidwspinner").hide();
     })
-    .fail(function(err){
-        console.error('error getting metar', err)
-        $("#eidwspinner").hide();
-        $("#eidw").append("error fetching METAR data");
-    })
+        .fail(function (err) {
+            console.error('error getting metar', err)
+            $("#eidwspinner").hide();
+            $("#eidw").append("error fetching METAR data");
+        })
 }
 
 function getBuoy() {
@@ -67,11 +67,11 @@ function getBuoy() {
         $("#buoy").append("<table class='table'>" + header + rows + "</table>")
         $("#buoyspinner").hide();
     })
-    .fail(function(err){
-        console.error('error getting Buoy data', err)
-        $("#buoyspinner").hide();
-        $("#buoy").append("error fetching Buoy weather data");
-    })
+        .fail(function (err) {
+            console.error('error getting Buoy data', err)
+            $("#buoyspinner").hide();
+            $("#buoy").append("error fetching Buoy weather data");
+        })
 }
 
 function getdlData() {
@@ -83,31 +83,32 @@ function getdlData() {
         var header = "<tr><th>Time</th><th>Temp</th><th>Wind</th><th>Dirn</th></tr>"
         var rows = '';
 
-        rows = rows.concat('<tr>' +
-            '<td>' + data[0].substr(18, 100) + '</td>' +
-            '<td>' + data[6].substr(17, 100).replace(/\s/g, '') + '</td>' +
-            '<td>' + parseFloat(data[2].split(":")[1].split('kts')[0])
-            + 'G' + parseFloat(data[3].split(":")[1].split('kts')[0])
-            + '</td>' +
-            '<td>' + data[1].split(":")[1].substr(1, 200).replace(/\s/g, '') + '</td>' +
-            '</tr>')
+        if (data.length > 0) {
+            rows = rows.concat('<tr>' +
+                '<td>' + data[0].substr(18, 100) + '</td>' +
+                '<td>' + data[6].substr(17, 100).replace(/\s/g, '') + '</td>' +
+                '<td>' + parseFloat(data[2].split(":")[1].split('kts')[0])
+                + 'G' + parseFloat(data[3].split(":")[1].split('kts')[0])
+                + '</td>' +
+                '<td>' + data[1].split(":")[1].substr(1, 200).replace(/\s/g, '') + '</td>' +
+                '</tr>')
 
-        $("#dldata").append("<table class='table'>" + header + rows + "</table>");
+            $("#dldata").append("<table class='table'>" + header + rows + "</table>");
 
-        $("#dldata").append("<p>" + data[15].replace(/\s\s/g, '') + '<br>' +
-            data[16].replace(/\s\s/g, '') + '<br>' +
-            data[17].replace(/\s\s/g, '') + '<br>' +
-            data[18].replace(/\s\s/g, '') + '<br>' +
-            "</p>");
+            $("#dldata").append("<p>" + data[15].replace(/\s\s/g, '') + '<br>' +
+                data[16].replace(/\s\s/g, '') + '<br>' +
+                data[17].replace(/\s\s/g, '') + '<br>' +
+                data[18].replace(/\s\s/g, '') + '<br>' +
+                "</p>");
 
-
+        }
         $("#dlspinner").hide();
     })
-    .fail(function(err){
-        console.error('error getting DL current weather', err)
-        $("#dlspinner").hide();
-        $("#dldata").append("error fetching DL current weather");
-    })
+        .fail(function (err) {
+            console.error('error getting DL current weather', err)
+            $("#dlspinner").hide();
+            $("#dldata").append("error fetching DL current weather");
+        })
 }
 
 function getdlWindData() {
@@ -128,11 +129,11 @@ function getdlWindData() {
         $("#dlwinddata").append("<table class='table'>" + header + rows + "</table>")
         $("#dlwindspinner").hide();
     })
-    .fail(function(err){
-        console.error('error getting DL weather data', err)
-        $("#dlwindspinner").hide();
-        $("#dlwinddata").append("error fetching DL weather data");
-    })
+        .fail(function (err) {
+            console.error('error getting DL weather data', err)
+            $("#dlwindspinner").hide();
+            $("#dlwinddata").append("error fetching DL weather data");
+        })
 }
 
 
@@ -140,7 +141,7 @@ function getdlWindData() {
 
 $(document).ready(function () {
     getWeather();
-    getBuoy();
+    // getBuoy();
     getdlData();
     getdlWindData();
 });
